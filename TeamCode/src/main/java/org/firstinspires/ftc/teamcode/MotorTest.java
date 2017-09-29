0c5061503ea941fefe5b02a5705127e4795337ce
Jojo <joangelvegan@gmail.com>
2017-09-29 10:37:06 -0500

    enabled Gage/Rafael again.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -53,8 +53,8 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Pushbot: Teleop Tank", group="Pushbot")
-// @Disabled
+@TeleOp(name="Gage/Rafael", group="Pushbot")
+@Disabled
 public class MotorTest extends OpMode{
 
     /* Declare OpMode members. */
@@ -101,7 +101,7 @@ public class MotorTest extends OpMode{
 
         // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
         left = gamepad1.left_stick_y;
-        right =gamepad1.right_stick_y;
+        right = gamepad1.right_stick_y;
 
         robot.leftMotor.setPower(left);
         robot.rightMotor.setPower(right);
