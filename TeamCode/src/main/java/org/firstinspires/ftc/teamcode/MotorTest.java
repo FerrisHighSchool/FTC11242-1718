b086d455f3dd8d7b67c5633cbc473cee5d62da8d
Jojo <joangelvegan@gmail.com>
2017-11-17 11:25:24 -0600

    changed "Rafael" TeleOp Mode back to "Gage/Rafael".

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -53,7 +53,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Rafael", group="Pushbot")
+@TeleOp(name="Gage/Rafael", group="Pushbot")
 // @Disabled
 public class MotorTest extends OpMode{
 
